2ca38f45e9d8b62be4b728db8ffa106b0a8caa17
Denmark Tenio <denmarktenio01@gmail.com>
2021-12-10 13:29:36 +0800

    Initial commit - pokemonSimulator activity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
 fun main (args: Array<String>) {
+    var input = ""
 
-    val bulbasaur = Pokemon("Bulbasaur","Grass")
-    println("New pokemon encountered: ${bulbasaur.name}")
-    println("Pokemon type: ${bulbasaur.type}")
+    val bulbasaur = Pokemon("Bulbasaur",1)
 
 
-    val charmander = Pokemon("Charmander", "Fire")
-    println("New pokemon encountered: ${charmander.name}")
-    println("Pokemon type: ${charmander.type}")
+
+
+    val charmander = Pokemon("Charmander", 2)
 
     fun startBattle (pokemon1: Pokemon, pokemon2: Pokemon) {
         while (!pokemon1.hasFainted() || !pokemon2.hasFainted()) {
@@ -28,5 +27,144 @@ fun main (args: Array<String>) {
         }
 
     }
-    startBattle(bulbasaur,charmander)
+//    startBattle(bulbasaur,charmander)
+    val squirtle = WaterTypePokemon("Squirtle", 3)
+//    println("Pokemon Level: ${squirtle.level}")
+//    println("Pokemon Attack: ${squirtle.attackPoints}")
+//    println("Pokemon Health: ${squirtle.healthPoints}")
+//
+//    println(squirtle.tackle())
+//    println(charmander.tackle())
+//    val treecko = GrassTypePokemon("Treecko", 5)
+//    println("Pokemon Level: ${treecko.level}")
+//    println("Pokemon Attack: ${treecko.attackPoints}")
+//    println("Pokemon Health: ${treecko.healthPoints}")
+//
+//    println(treecko.vineWhip())
+//    println(treecko.tackle())
+    val pickachu = ElectricTypePokemon("Pikachu", 3)
+    val torchic = FireTypePokemon("Torchic", 3)
+
+
+    fun pokemonSim (pokemon1: ElectricTypePokemon, pokemon2: FireTypePokemon) {
+        while (!pokemon1.hasFainted() || !pokemon2.hasFainted()) {
+            // FIRST TURN
+            println("${pokemon1.name}'s turn!")
+            println("Health: ${pokemon1.healthPoints}")
+            println("Stamina: ${pokemon1.stamina}")
+            println("Which move will you use?")
+            println("TACKLE  THUNDERCAGE  THUNDERPUNCH  THUNDERBOLT")
+            input = readLine()!!
+            when (input.lowercase()) {
+                "tackle" -> {
+                    println("${pokemon1.name} used TACKLE!")
+                    pokemon2.healthPoints -= pokemon1.tackle()
+                    println("${pokemon2.name}'s health is now down to ${pokemon2.healthPoints}")
+                }
+                "thundercage" -> {
+                    while (pokemon1.stamina < 1) {
+                        println("Insufficient stamina.")
+                        println("What move will you use?")
+                        println("TACKLE  THUNDERCAGE  THUNDERPUNCH  THUNDERBOLT")
+                        input = readLine()!!
+                    }
+                    println("${pokemon1.name} used THUNDERCAGE!")
+                    pokemon2.healthPoints -= pokemon1.thunderCage()
+                    println("${pokemon2.name}'s health is now down to ${pokemon2.healthPoints}")
+                }
+                "thunderpunch" -> {
+                    while (pokemon1.stamina < 2) {
+                        println("Insufficient stamina.")
+                        println("What move will you use?")
+                        println("TACKLE  THUNDERCAGE  THUNDERPUNCH  THUNDERBOLT")
+                        input = readLine()!!
+                    }
+                    println("${pokemon1.name} used THUNDERPUNCH!")
+                    pokemon2.healthPoints -= pokemon1.thunderPunch()
+                    println("${pokemon2.name}'s health is now down to ${pokemon2.healthPoints}")
+                }
+                "thunderbolt" -> {
+                    while (pokemon1.stamina < 3) {
+                        println("Insufficient stamina.")
+                        println("What move will you use?")
+                        println("TACKLE  THUNDERCAGE  THUNDERPUNCH  THUNDERBOLT")
+                        input = readLine()!!
+                    }
+                    println("${pokemon1.name} used THUNDERBOLT!")
+                    pokemon2.healthPoints -= pokemon1.thunderBolt()
+                    println("${pokemon2.name}'s health is now down to ${pokemon2.healthPoints}")
+                }
+                else -> {
+                    println("INVALID MOVE")
+                    println("Which move will you use?")
+                    println("TACKLE  THUNDERCAGE  THUNDERPUNCH  THUNDERBOLT")
+                    input = readLine()!!
+                }
+            }
+            if (pokemon2.hasFainted()) {
+                println("${pokemon2.name} fainted. ${pokemon1.name} won!")
+                break
+            }
+            // SECOND TURN
+            println("${pokemon2.name}'s turn!")
+            println("Health: ${pokemon2.healthPoints}")
+            println("Stamina: ${pokemon2.stamina}")
+            println("Which move will you use?")
+            println("TACKLE  EMBER  FIREBLAST  FLAMETHROWER")
+            input = readLine()!!
+            when (input.lowercase()) {
+                "tackle" -> {
+                    println("${pokemon2.name} used TACKLE!")
+                    pokemon1.healthPoints -= pokemon2.tackle()
+                    println("${pokemon1.name}'s health is now down to ${pokemon1.healthPoints}")
+                }
+                "ember" -> {
+                    while (pokemon2.stamina < 1) {
+                        println("Insufficient stamina.")
+                        println("What move will you use?")
+                        println("TACKLE  EMBER  FIREBLAST  FLAMETHROWER")
+                        input = readLine()!!
+                    }
+                    println("${pokemon2.name} used EMBER!")
+                    pokemon1.healthPoints -= pokemon2.ember()
+                    println("${pokemon1.name}'s health is now down to ${pokemon1.healthPoints}")
+                }
+                "fireblast" -> {
+                    while (pokemon2.stamina < 2) {
+                        println("Insufficient stamina.")
+                        println("What move will you use?")
+                        println("TACKLE  EMBER  FIREBLAST  FLAMETHROWER")
+                        input = readLine()!!
+                    }
+                    println("${pokemon2.name} used FIREBLAST!")
+                    pokemon1.healthPoints -= pokemon2.fireBlast()
+                    println("${pokemon1.name}'s health is now down to ${pokemon1.healthPoints}")
+                }
+                "flamethrower" -> {
+                    while (pokemon2.stamina < 3) {
+                        println("Insufficient stamina.")
+                        println("What move will you use?")
+                        println("TACKLE  EMBER  FIREBLAST  FLAMETHROWER")
+                        input = readLine()!!
+                    }
+                    println("${pokemon2.name} used FLAMETHROWER!")
+                    pokemon1.healthPoints -= pokemon2.flameThrower()
+                    println("${pokemon1.name}'s health is now down to ${pokemon1.healthPoints}")
+                }
+                else -> {
+                    println("INVALID MOVE")
+                    println("Which move will you use?")
+                    println("TACKLE  EMBER  FIREBLAST  FLAMETHROWER")
+                    input = readLine()!!
+                }
+            }
+            if (pokemon1.hasFainted()) {
+                println("${pokemon1.name} fainted. ${pokemon2.name} won!")
+                break
+            }
+
+        }
+    }
+    pokemonSim(pickachu,torchic)
+
 }
--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -1,6 +1,7 @@
-class Pokemon(val name: String, val type: String) {
+open class Pokemon(val name: String, var level: Int) {
     var healthPoints = 100
     var attackPoints = 10
+
 
     fun hasFainted(): Boolean {
         return healthPoints <= 0
@@ -8,7 +9,76 @@ class Pokemon(val name: String, val type: String) {
 
 
     // Methods
-    fun tackle () {
-        println("$name attacked with $attackPoints points.")
+    open fun tackle (): Int {
+//        println("$name used Tackle!")
+        return attackPoints/2
+    }
+
+    // init runs before anything else
+    init {
+        this.healthPoints *= this.level
+        this.attackPoints *= this.level
+    }
+}
+
+// Inheritance
+// We can set up
+class WaterTypePokemon (name: String, level: Int): Pokemon(name, level) {
+    fun hydroPump (): Int {
+        return attackPoints / 2
+    }
+    override fun tackle (): Int {
+//        println("$name used Tackle!")
+        return attackPoints + 10
+    }
+}
+class GrassTypePokemon (name: String, level: Int): Pokemon(name, level) {
+    fun vineWhip (): Int {
+//        println("$name used Vine Whip!!")
+        return attackPoints + 5
+    }
+    override fun tackle (): Int {
+//        println("$name used Tackle!")
+        return attackPoints + 20
+    }
+}
+
+class FireTypePokemon (name: String, level: Int): Pokemon(name,level) {
+    var stamina = 3
+    override fun tackle (): Int {
+        this.stamina += 1
+        return attackPoints/2
+    }
+    fun ember(): Int {
+        this.stamina -= 1
+        return attackPoints + 2
+    }
+    fun fireBlast(): Int {
+        this.stamina -= 2
+        return attackPoints + 3
+    }
+    fun flameThrower(): Int {
+        this.stamina -= 3
+        return attackPoints + 4
+    }
+}
+
+class ElectricTypePokemon (name: String, level: Int): Pokemon(name,level) {
+    var stamina = 3
+    override fun tackle (): Int {
+        this.stamina += 1
+        return attackPoints/2
+    }
+    fun thunderCage(): Int {
+        this.stamina -= 1
+        return attackPoints + 3
+    }
+    fun thunderPunch(): Int {
+        this.stamina -= 2
+        return attackPoints + 4
+    }
+    fun thunderBolt(): Int {
+        this.stamina -= 3
+        return attackPoints + 5
     }
 }
